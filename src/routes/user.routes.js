3e8ac5856866318cb33c8de29632b6bbdc068e00
Abhishek Chauhan <124580596+abhishekcha790@users.noreply.github.com>
2025-02-17 23:52:27 +0530

    routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,8 @@ import {
   loginUser,
   registerUser,
   logoutUser,
+  changeCurrentPassword,
+  getUserChannel,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -22,5 +24,7 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/username").patch(verifyJwt, getUserChannel);
 
 export default router;
